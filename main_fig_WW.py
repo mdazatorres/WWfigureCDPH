55b57d42783f16494e1ac8c843a2feafe2c362c6
marialuisa <mdazatorres@cimat.mx>
2022-08-11 13:41:59 -0700

    We add the psotivity rate

--- main_fig_WW.py
+++ main_fig_WW.py
@@ -10,7 +10,7 @@ import numpy as np
 import streamlit as st
 from plot_conc import  plotN_CV
 from aux_function import is_authenticated, generate_login_block, clean_blocks, login
-from datetime import datetime, timedelta
+from datetime import  timedelta
 
 
 st.set_page_config(layout="wide", initial_sidebar_state="auto", page_title=None, page_icon=None)
@@ -27,59 +27,43 @@ def read_data():
     data = city_data[city_data['SampleDate'] > '2021-09-30']
     return data
 
-#return pd.read_csv('data_ww_cases_full.csv')
+
+with open('styles.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+city_data = read_data()
+
+st.header('Healthy Central Valley Together: Wastewater SARS-CoV-2 data for Central Valley')
+
+city_data['SampleDate'] = pd.to_datetime(city_data['SampleDate'])
+start_date = city_data['SampleDate'].min().to_pydatetime()
+end_date = city_data['SampleDate'].max().to_pydatetime()
 
 
-def main():
-    with open('styles.css') as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+st.sidebar.header('PLOT DETAILS')
+col1, col2 = st.sidebar.columns([1, 0.51])
+smooth = col1.selectbox('Smoothing function', ['Trimmed average', 'Moving average', 'None'], index=1)
 
-    city_data = read_data()
+if smooth != 'None':
+    size_window = col2.selectbox('Size window', [5,7,10,14], index=1)
+    center = col1.checkbox('Center')
+else:
+    size_window = 0
+    center = False
 
-    st.header('Healthy Central Valley Together: Wastewater SARS-CoV-2 data for Central Valley')
+log = st.sidebar.checkbox('Log transformation')
 
-    city_data['SampleDate'] = pd.to_datetime(city_data['SampleDate'])
-    start_date = city_data['SampleDate'].min().to_pydatetime()
-    end_date = city_data['SampleDate'].max().to_pydatetime()
+col1, col2, col3 = st.columns([4, 0.2, 1])
 
+sl_init, sl_end = col1.slider('', min_value=start_date, max_value=end_date+timedelta(days = 0), value=(start_date, end_date+timedelta(days =0)),format='MMM DD, YYYY')
 
-    st.sidebar.header('PLOT DETAILS')
-    col1, col2 = st.sidebar.columns([1, 0.51])
-    smooth = col1.selectbox('Smoothing function', ['Trimmed average', 'Moving average', 'None'], index=1)
+col1, col2, col3 = st.columns([4, 0.2, 1])
+figCV = plotN_CV(city_data, sl_init, sl_end, log, smooth, size_window, center, cities_solids, cts_label)
+col1.plotly_chart(figCV, use_container_width=True)
 
-    if smooth != 'None':
-        size_window = col2.selectbox('Size window', [5,7,10,14], index=1)
-        center = col1.checkbox('Center')
-    else:
-        size_window = 0
-        center = False
-
-    log = st.sidebar.checkbox('Log transformation')
-
-    col1, col2, col3 = st.columns([4, 0.2, 1])
-
-    sl_init, sl_end = col1.slider('', min_value=start_date, max_value=end_date+timedelta(days = 0), value=(start_date, end_date+timedelta(days =0)),format='MMM DD, YYYY')
-
-    col1, col2, col3 = st.columns([4, 0.2, 1])
-    figCV = plotN_CV(city_data, sl_init, sl_end, log, smooth, size_window, center, cities_solids, cts_label)
-    col1.plotly_chart(figCV, use_container_width=True)
-
-    st.markdown(""" <style>[data-testid="stSidebar"][aria-expanded="true"] > div:first-child {width: 280px;}
-        [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {width: 280px;margin-left: -300px;}
-        </style>""", unsafe_allow_html=True, )
-
-
-login_blocks = generate_login_block()
-password = login(login_blocks)
-
-if is_authenticated(password):
-    clean_blocks(login_blocks)
-    main()
-elif password:
-    st.info("Please enter a valid password")
-
-
-
+st.markdown(""" <style>[data-testid="stSidebar"][aria-expanded="true"] > div:first-child {width: 280px;}
+    [data-testid="stSidebar"][aria-expanded="false"] > div:first-child {width: 280px;margin-left: -300px;}
+    </style>""", unsafe_allow_html=True, )
 
 
 
--- plot_conc.py
+++ plot_conc.py
@@ -1,29 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb  9 17:37:42 2022
 
-@author: marialuisa
-"""
-
-# import pandas as pd
 import numpy as np
-# import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from scipy.stats import trim_mean, pearsonr
-from sklearn.linear_model import LinearRegression
 import plotly.express as px
-
-
-
-# double click on legend to isolete the trace
-
-def trim_fun1(x):
-    x1 = x[x != x.max()]  # Remove max values
-    x2 = x1[x1 != x1.min()]  # Remove min values
-    return x2.mean(skipna=True)
 
 
 def trim_fun(x):
@@ -44,7 +24,6 @@ def plotN_CV(data_ww, start_date, end_date, log, opt, size_window, center, cts, 
     sw = size_window-1
     swc = size_window//2
 
-
     for i in range(n):
         city_data_new = data_ww[data_ww['City'] == cts[i]]
         city_data = city_data_new[(city_data_new['SampleDate'] >= start_date) & (city_data_new['SampleDate'] <= end_date)]
@@ -56,7 +35,6 @@ def plotN_CV(data_ww, start_date, end_date, log, opt, size_window, center, cts, 
             Y = city_data['NormalizedConc_wo'].rolling(window=size_window, center=center, min_periods=min_period).mean()
             if center:
                 fig.add_trace(go.Scatter(name=cts_label[cts[i]], mode='lines', x=X[swc:-swc], y=Y[swc:-swc], line=dict(color=color[i], width=3)), secondary_y=False, )
-
             else:
                 fig.add_trace(go.Scatter(name=cts_label[cts[i]], mode='lines', x=X[sw-1:], y=Y[sw-1:], line=dict(color=color[i], width=3)), secondary_y=False, )
         elif opt == 'Trimmed average':  # .apply(lambda x: trim_mean(x, 0.2))
@@ -77,7 +55,6 @@ def plotN_CV(data_ww, start_date, end_date, log, opt, size_window, center, cts, 
                       legend_font_size=14, legend_title_font_size=fontsize)
     fig.update_layout(legend=dict(orientation="h", yanchor="bottom", y=-0.6, xanchor="right", x=0.9))
     fig.update_layout({'plot_bgcolor': 'rgba(0,0,0,0)', 'paper_bgcolor': 'rgba(0,0,0,0)'})
-
     fig.update_layout(autosize=False, width=450, height=250, margin=dict(l=0, r=0, b=10, t=10, pad=4),
                       yaxis=dict(title='N gene / PMMoV'))  # ,xaxis=dict(title="Date"))
     fig.update_layout(font_family="Arial", title_font_family="Arial")
